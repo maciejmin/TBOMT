7ecd29b17dd5cbc84eb149113a2385354a80244e
maciejmin <maciej@linuxmail.org>
2026-04-23 14:34:16 +0200

    Refactor update check logic in program.py

--- program.py
+++ program.py
@@ -35,7 +35,6 @@ def createfile(localization_or_name,what_to_write=None,request_link=None,how_to_
     file.close()
 easygui.msgbox("Uwaga! Gra najlepiej działa na Linuxie i nie jest zalecana dla osób z epilepsją fotogenną oraz w wieku poniżej 13 lat ponieważ zawiera szybkie animacje powodujące nienadążający wzrok za efektami u młodszych osób.")
 easygui.msgbox("Jeżeli znajdziesz jakikolwiek błąd zgłoś nam to na maila the_beginning_of_modern_times@galaxyhit.com a my spróbujemy to naprawić!")
-w = easygui.buttonbox("Czy chcesz sprawdzić aktualizacje gry? Jeżeli będzie taka możliwość zaaktualizujemy automatycznie program. To wymaga połączenia internetowego co może powodować opłaty."," ",["Tak","Nie"])
 def updating():
     import updater
     respond = updater.update_program()
@@ -70,8 +69,6 @@ def updating():
                 if w == None:
                     easygui.msgbox("Bye!")
                     exit()
-if w == "Tak":
-    updating()
 print(os.listdir())
 if len(os.listdir()) == 1:
     print("It's ok, we're only installing important thinks, you can find it below. Do not close this frame please.")
@@ -112,6 +109,9 @@ except:
             print("Package can't be installed. We must to kill the process.")
             exit()
 #szukanie pliku uninstall.py
+w = easygui.buttonbox("Czy chcesz sprawdzić aktualizacje gry? Jeżeli będzie taka możliwość zaaktualizujemy automatycznie program. To wymaga połączenia internetowego co może powodować opłaty."," ",["Tak","Nie"])
+if w == "Tak":
+    updating()
 if os.path.exists("uninstall.py"):
     w = easygui.buttonbox("Jesteś w zapisanym")
     print("uninstalling!")
