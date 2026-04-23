4a79711e139ddf630423f9fb40a5cdc347e7ff7f
maciejmin <maciej@linuxmail.org>
2026-04-23 15:46:17 +0200

    Update sources_adder.py

--- sources_adder.py
+++ sources_adder.py
@@ -1,1 +1,20 @@
-#vTest
+#vTest|https://raw.githubusercontent.com/maciejmin/TBOMT/refs/heads/main/sources_adder.py
+#program służy do pobrania extensions z katalogu extensions i ich wrzut do pliku sources.list oraz z tego folderu
+import os
+if os.name == "nt":
+    slash = "\\"
+else:
+    slash = "/"
+files = os.listdir(".")
+sources = open("sources.list","w+")
+for i in files:
+    if i[-3:] == ".py": #ten plik pobieramy
+        file = open(i,"r")
+        sources.write(file.read().splitlines()[0][1:]+"|"+i+"\n")
+        file.close()
+files = os.listdir(os.getcwd()+slash+"extensions")
+for i in files:
+    if i[-3:] == ".py": #ten plik pobieramy
+        file = open(i,"r")
+        sources.write(file.read().splitlines()[0][1:]+"|"+i+"\n")
+        file.close()
