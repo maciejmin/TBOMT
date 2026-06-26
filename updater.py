abbae77f0d2519e48e6dd5ee312b8d5ccee89988
maciejmin <maciej@linuxmail.org>
2026-06-26 16:24:06 +0200

    Add UTF-8 encoding to file operations in updater.py

--- updater.py
+++ updater.py
@@ -1,9 +1,9 @@
-#vTest|https://raw.githubusercontent.com/maciejmin/TBOMT/refs/heads/main/updater.py
+#v1.0|https://raw.githubusercontent.com/maciejmin/TBOMT/refs/heads/main/updater.py
 #jezeli jakis dodatek na sources.list nie ma dwóch | nalezy go pominac
 def update_program():
     print("Uruchamiam uaktualniacz, sprawdzam aktualizacje za pomocą funkcji requests oraz repozytorium Github TBOMT, czekaj...")
     try:
-        file = open("program.py","r+")
+        file = open("program.py","r+",encoding="utf-8")
     except Exception as e:
         print("Wygląda na to, że wystąpił błąd z pobieraniem pliku z katalogu. Może się okazać, że uruchamiasz np. w VS CODE, może to powodować, iż program próbuje pobrać z innego katalogu niż z tego. Upewnij się, że otwierasz program.py w miejscu programu.")
         print("Przerywam działanie programu w tym momencie.")
@@ -29,7 +29,7 @@ def update_program():
     else:
         #aktualizujemy
         file.close()
-        file = open("program.py","w+")
+        file = open("program.py","w+",encoding="utf-8")
         file.write(web_file)
         file.close()
         print("Powodzenie.")
@@ -39,11 +39,11 @@ def update_extensions():
     import requests
     print("searching from sources.list")
     try:
-        file = open("sources.list","r")
+        file = open("sources.list","r",encoding="utf-8")
     except Exception as e:
-        return ["40, updater.py: failed opening sources.list.",str(e)]
+        return ["44, updater.py: failed opening sources.list.",str(e)]
     sources = file.read().splitlines()
-    file.close
+    file.close()
     everyone = [] #wszystkie ze zwrotem informacji
     for i in sources: #plik caly zrodel
         current = "" #jeden z listy
@@ -60,12 +60,12 @@ def update_extensions():
             lista.append(current)
             zrodlo = requests.get(lista[1]).text
             wersja_zrodla = zrodlo.splitlines()[0]
-            plik = open(lista[2],"r").read().splitlines()[0]
+            plik = open(lista[2],"r",encoding="utf-8").read().splitlines()[0]
             if wersja_zrodla == plik:
                 everyone.append(str(i)+": Up to date.")
             else:
                 try:
-                    file = open(i,"w+")
+                    file = open(i,"w+",encoding="utf-8")
                     file.write(zrodlo)
                     file.close()
                     everyone.append(str(i)+": Updated to "+wersja_zrodla+" from "+plik+"!")
