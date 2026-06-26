67cdfb52c25fe4e2d7ce1500f1bc663e0ee491fd
maciejmin <maciej@linuxmail.org>
2026-06-26 16:25:49 +0200

    Add UTF-8 encoding to file operations in sources_adder.py
    Updated file handling to use UTF-8 encoding for reading and writing sources.list.

--- sources_adder.py
+++ sources_adder.py
@@ -1,4 +1,4 @@
-#vTest|https://raw.githubusercontent.com/maciejmin/TBOMT/refs/heads/main/sources_adder.py
+#v1.0|https://raw.githubusercontent.com/maciejmin/TBOMT/refs/heads/main/sources_adder.py
 #program służy do pobrania extensions z katalogu extensions i ich wrzut do pliku sources.list oraz z tego folderu
 import os
 if os.name == "nt":
@@ -6,15 +6,15 @@ if os.name == "nt":
 else:
     slash = "/"
 files = os.listdir(".")
-sources = open("sources.list","w+")
+sources = open("sources.list","w+",encoding="utf-8")
 for i in files:
     if i[-3:] == ".py": #ten plik pobieramy
-        file = open(i,"r")
+        file = open(i,"r",encoding="utf-8")
         sources.write(file.read().splitlines()[0][1:]+"|"+i+"\n")
         file.close()
 files = os.listdir(os.getcwd()+slash+"extensions")
 for i in files:
     if i[-3:] == ".py": #ten plik pobieramy
-        file = open(i,"r")
+        file = open(i,"r",encoding="utf-8")
         sources.write(file.read().splitlines()[0][1:]+"|"+i+"\n")
         file.close()
