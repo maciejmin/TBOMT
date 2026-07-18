f7ee3cd873c4b4753cbbda48e063ed64909e79bb
maciejmin <maciej@linuxmail.org>
2026-07-18 14:31:10 +0200

    Add initial implementation of TBOMT extension compiler
    Implement a compiler for managing TBOMT extensions with a GUI.

---
+++ compiler.py
@@ -0,0 +1,39 @@
+#vtest
+#program czyta extensions.todo i wsadza pliki do programu
+print("Compiler initialized!")
+def do():
+    import easygui
+    def compiler():
+        w = easygui.indexbox("Witaj w kompilerze dodatków TBOMT, wybierz co teraz chcesz zrobić."," ",["Zaaktualizuj listę dodatków (Jeżeli ostatnio pobierano)","Przejrzyj listę i wgraj do gry"])
+        if w == 0:
+            print("uruchomię w tym celu extensiondownloader.py.")
+            try:
+                import extensiondownloader
+                easygui.msgbox("Wydaje się, że zaaktualizowano listę dodatków.")
+                compiler()
+            except Exception as e:
+                w = easygui.indexbox("Coś poszło nie tak. Możliwe, że cała lista dodatków się wysypała. Jeżeli wszystko jest ok z dodatkami nie przejmuj się tym, mimo tego możesz się znami skontaktować jeżeli to błąd."," ",["Szczegóły dla programisty","Skontaktuj się z nami","To nie istotne"])
+                if w == 0:
+                    easygui.codebox("Poniżej szczegóły dla programisty:"," ",e)
+                elif w == 1:
+                    easygui.codebox("Wyślij na maila the_beginning_of_modern_times@galaxyhit.com"," ",e)
+                else:
+                    compiler() #uruchamiamy ponownie
+        elif w == 1:
+            file = open("extensions.todo","r",encoding="utf-8")
+            dodatki = file.read()
+            try:
+                w = easygui.choicebox("Wybierz dodatek i nim zarządzaj!"," ",dodatki.splitlines())
+            except:
+                w = easygui.codebox("Wybierz dodatek, i nim zarządzaj. Zmień konkretną linię z dodatkiem, którym chcesz zarządzać np. zamiast \"noise.py|basic extension\", wpisz w tym miejscu \"example|example\". Uwaga! Możesz edytować tylko jeden jednocześnie."," ",dodatki)
+                if w == dodatki:
+                    w = easygui.indexbox("Wygląda na to, że nie chcesz edytować żadnego dodatku. Co teraz?"," ",["Wróć do TBOMT","Rozpocznij na nowo edycję dodatków"])
+                    if w == 1:
+                        compiler()
+                else:        
+                    w = w.splitlines()
+                    if len(w) == len(dodatki.splitlines()): #gracz nie edytował zbyt dużo
+                        for i in range(len(w)):
+                            if w != w:
+                                pass
+    compiler()
