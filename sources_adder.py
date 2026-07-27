24a00b024c50aba5ba79a744fbb84f7920bd3637
maciejmin <maciej@linuxmail.org>
2026-07-27 19:06:26 +0200

    updated to release version and added posibility to update updater

--- sources_adder.py
+++ sources_adder.py
@@ -1,20 +1,26 @@
-#v1.0|https://raw.githubusercontent.com/maciejmin/TBOMT/refs/heads/main/sources_adder.py
+#v1.1|https://raw.githubusercontent.com/maciejmin/TBOMT/refs/heads/main/sources_adder.py
 #program służy do pobrania extensions z katalogu extensions i ich wrzut do pliku sources.list oraz z tego folderu
 import os
-if os.name == "nt":
-    slash = "\\"
-else:
-    slash = "/"
-files = os.listdir(".")
-sources = open("sources.list","w+",encoding="utf-8")
-for i in files:
-    if i[-3:] == ".py": #ten plik pobieramy
-        file = open(i,"r",encoding="utf-8")
-        sources.write(file.read().splitlines()[0][1:]+"|"+i+"\n")
-        file.close()
-files = os.listdir(os.getcwd()+slash+"extensions")
-for i in files:
-    if i[-3:] == ".py": #ten plik pobieramy
-        file = open(i,"r",encoding="utf-8")
-        sources.write(file.read().splitlines()[0][1:]+"|"+i+"\n")
-        file.close()
+import requests
+def refresh():
+    if os.name == "nt":
+        slash = "\\"
+    else:
+        slash = "/"
+    files = os.listdir(".")
+    sources = open("sources.list","w+",encoding="utf-8")
+    for i in files:
+        if i[-3:] == ".py": #ten plik pobieramy
+            file = open(i,"r",encoding="utf-8")
+            sources.write(file.read().splitlines()[0][1:]+"|"+i+"\n")
+            file.close()
+    files = os.listdir(os.getcwd()+slash+"extensions")
+    for i in files:
+        if i[-3:] == ".py": #ten plik pobieramy
+            file = open(i,"r",encoding="utf-8")
+            sources.write(file.read().splitlines()[0][1:]+"|"+i+"\n")
+            file.close()
+def update_updater():
+    file = open("updater.py","w+",encoding="utf-8")
+    file.write(requests.get("https://raw.githubusercontent.com/maciejmin/TBOMT/refs/heads/main/updater.py").text)
+    file.close()
