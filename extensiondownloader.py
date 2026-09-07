edc10871f8ed901e003ef8a0befbc15c3f1d055e
maciejmin <maciej@linuxmail.org>
2026-09-07 21:36:14 +0200

    fixed requests.get

--- extensiondownloader.py
+++ extensiondownloader.py
@@ -1,11 +1,12 @@
-#v0.2.3|https://raw.githubusercontent.com/maciejmin/TBOMT/refs/heads/main/extensiondownloader.py
+#v0.2.4|https://raw.githubusercontent.com/maciejmin/TBOMT/refs/heads/main/extensiondownloader.py
 #program służy do pobrania extensions z katalogu extensions, następnie wgranie go do pliku extensions.todo, który zawiera każdą linię itd. w której która extension. Potem otwiera compiler.py który wysyła wszystko do program.py ale przed tem tqorzy kopię zapasową tego poprzedniego pliku.
 import os
 try:
     import addons_settings_adder
 except:
+    import requests
     file = open("addons_settings_adder.py","w+",encoding="utf-8")
-    file.write("https://raw.githubusercontent.com/maciejmin/TBOMT/refs/heads/main/addons_settings_adder.py")
+    file.write(requests.get("https://raw.githubusercontent.com/maciejmin/TBOMT/refs/heads/main/addons_settings_adder.py").text)
     file.close()
     import addons_settings_adder
 if os.name == "nt":
