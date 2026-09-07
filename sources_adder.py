6ea8483e8c6ceb73b163240a10679c9173ac5f45
maciejmin <maciej@linuxmail.org>
2026-09-07 21:11:32 +0200

    Update sources_adder.py for better error handling
    Refactor sources refresh logic to handle extensions directory and error handling more gracefully.

--- sources_adder.py
+++ sources_adder.py
@@ -1,4 +1,4 @@
-#v1.1.1|https://raw.githubusercontent.com/maciejmin/TBOMT/refs/heads/main/sources_adder.py
+#v1.1.2|https://raw.githubusercontent.com/maciejmin/TBOMT/refs/heads/main/sources_adder.py
 #program służy do pobrania extensions z katalogu extensions i ich wrzut do pliku sources.list oraz z tego folderu
 import os
 import requests
@@ -7,19 +7,30 @@ def refresh():
         slash = "\\"
     else:
         slash = "/"
-    files = os.listdir(".")
-    sources = open("sources.list","w+",encoding="utf-8")
-    for i in files:
-        if i[-3:] == ".py" and "__init__.py" != i: #ten plik pobieramy
-            file = open(i,"r",encoding="utf-8")
-            sources.write(file.read().splitlines()[0][1:]+"|"+i+"\n")
+    try:
+        files = os.listdir(".")
+        sources = open("sources.list","w+",encoding="utf-8")
+        for i in files:
+            if i[-3:] == ".py" and "__init__.py" != i: #ten plik pobieramy
+                file = open(i,"r",encoding="utf-8")
+                sources.write(file.read().splitlines()[0][1:]+"|"+i+"\n")
+                file.close()
+        files = os.listdir(os.getcwd()+slash+"extensions")
+        for i in files:
+            if i[-3:] == ".py" and i != "__init__.py": #ten plik pobieramy
+                file = open(i,"r",encoding="utf-8")
+                sources.write(file.read().splitlines()[0][1:]+"|"+i+"\n")
+                file.close()
+    except:
+        print("Błąd, prawdopodobnie nie ma folderu z dodatkami. Zostanie on zrobiony!")
+        try:
+            import extensionsinstaller
+        except:
+            file = open("extensionsinstaller.py","w+",encoding="utf-8")
+            file.write("https://raw.githubusercontent.com/maciejmin/TBOMT/refs/heads/main/extensionsinstaller.py")
             file.close()
-    files = os.listdir(os.getcwd()+slash+"extensions")
-    for i in files:
-        if i[-3:] == ".py" and i != "__init__.py": #ten plik pobieramy
-            file = open(i,"r",encoding="utf-8")
-            sources.write(file.read().splitlines()[0][1:]+"|"+i+"\n")
-            file.close()
+            import extensionsinstaller
+        extensionsinstaller.do()
 def update_updater():
     file = open("updater.py","w+",encoding="utf-8")
     file.write(requests.get("https://raw.githubusercontent.com/maciejmin/TBOMT/refs/heads/main/updater.py").text)
