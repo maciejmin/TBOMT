ecc225797f6d8907ce8a6e28227294cea22fb96a
maciejmin <maciej@linuxmail.org>
2026-09-20 19:16:39 +0200

    Create installer/windows/installer.bat

---
+++ installer/windows/installer.bat
@@ -0,0 +1,6 @@
+cls
+@echo off
+echo Instalacja TBOMT, poczekaj to może potrwać do kilku minut na starszych urządzeniach. W tym czasie zainstalujemy zależności i uruchomimy grę automatycznie!
+winget install python3
+curl -O "https://raw.githubusercontent.com/maciejmin/TBOMT/refs/heads/main/program.py"
+python3 program.py
