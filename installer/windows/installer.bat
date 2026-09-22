7c396a67bf91cc3e9bc52ac7ffe7eaf8023ccc83
maciejmin <maciej@linuxmail.org>
2026-09-22 16:43:07 +0200

    I think python3 command is wrong, better way is using python command for Windows.

--- installer/windows/installer.bat
+++ installer/windows/installer.bat
@@ -3,4 +3,4 @@ cls
 echo Instalacja TBOMT, poczekaj to może potrwać do kilku minut na starszych urządzeniach. W tym czasie zainstalujemy zależności i uruchomimy grę automatycznie!
 winget install python3
 curl -O "https://raw.githubusercontent.com/maciejmin/TBOMT/refs/heads/main/program.py"
-python3 program.py
+python program.py
